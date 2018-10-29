be7e5e89298ecdfd52e9dfdfc309cabbdf5046dc
unknown <fujianfeng4@360buyad.local>
2018-10-29 15:51:51 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("my name is fujianfeng");  //我的名字是 付剑锋
+
+    }
+
+}
